067c58a691291329484d89788de9fb657d98b936
wweidler <weidlerwill@gmail.com>
2022-02-14 19:46:41 -0600

    Update functions

--- lab3.cpp
+++ lab3.cpp
@@ -19,29 +19,31 @@ void trick(int attackVal, int healthVal);
 void leave(int attackVal, int healthVal, int expVal);
 
 int main(){
-        greeting();
-        cout << "What is your attack?" << endl;
-        cin >> attackVal;
-        cout << "What is your health?" << endl;
-        cin >> healthVal;
-        while (healthVal > 0){
-                cout << "What will you do?" << endl << "  1. Slice" << endl << "  2. Spin" << endl << "  3. Trick" << endl << "  4. Leave" << endl;
-                string option;
-                cin >> option;
-                if (option == "Slice"){
-                        slice(attackVal);
-                }
-                else if (option == "Spin"){
-                        spin(attackVal);
-                }
-                else if (option == "Trick"){
-                        trick(attackVal, healthVal);
-                }
-                else if (option == "Leave"){
-                        leave(attackVal, healthVal, expVal);
-                }
+    greeting();
+    cout << "What is your attack?" << endl;
+    cin >> attackVal;
+    cout << "What is your health?" << endl;
+    cin >> healthVal;
+    while (healthVal > 0){
+        cout << "What will you do?" << endl << "  1. Slice" << endl << "  2. Spin" << endl << "  3. Trick" << endl << "  4. Leave" << endl;
+        string option = "";
+        cin >> option;
+        if (option == "Slice"){
+            slice(attackVal);
         }
-        return 0;
+        else if (option == "Spin"){
+            spin(attackVal);
+        }
+        else if (option == "Trick"){
+            trick(attackVal, healthVal);
+        }
+        else if (option == "Leave"){
+            leave(attackVal, healthVal, expVal);
+            }
+    }
+    cout << "You have died." << endl;
+    leave(attackVal, healthVal, expVal);
+    return 0;
 }
 
 void greeting(){
@@ -56,13 +58,23 @@ void slice(int attackVal){
         cout << "Your attack was too low. You lost 1 health." << endl;
         healthVal--;
     }
+    return;
 }
 void spin(int attackVal){
+    if (attackVal < 10){
+        cout << "Your attack was too low. You lost 10 health" << endl;
+        healthVal -= 10;
+    }
     return;
 }
 void trick(int attackVal, int healthVal){
+    int temp;
+    attackVal = temp;
+    healthVal = attackVal;
+    healthVal = temp;
     return;
 }
 void leave(int attackVal, int healthVal, int expVal){
+    cout << "Normo:" << endl << "Attack - " << attackVal << endl << "Health - " << healthVal << endl << "Exp - " << expVal;
     return;
 }
